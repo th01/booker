e99b55515d2e1001285a1832c2c0e1154f4f66fd
Travis Hooper <travis.hooper@peopleadmin.com>
2016-02-08 15:47:22 -0600

    Adds working auth

--- booker.rb
+++ booker.rb
@@ -7,9 +7,13 @@ class Booker < Sinatra::Base
   set :public_folder => "public", :static => true
 
   use Rack::Auth::Basic, "Restricted Area" do |username, password|
-    binding.pry
     endpoint = ENV.fetch('MS_EXCHANGE_SERVER_URL')
     client   = Viewpoint::EWSClient.new(endpoint, username, password)
+    begin
+      !!client.folders
+    rescue Viewpoint::EWS::Errors::UnauthorizedResponseError => e
+      false
+    end
   end
 
   get "/" do
