2b582c7ecb9ca4d232a6a5d6caba79a6609eba2d
Travis Hooper <travis.hooper@peopleadmin.com>
2016-02-09 11:53:42 -0600

    Adds basic login

--- Gemfile
+++ Gemfile
@@ -3,13 +3,13 @@ source "https://rubygems.org/"
 # App Stack
 gem 'bundler'
 gem "sinatra", "~> 1.4"
-gem 'pry'
 gem 'activesupport'
 gem 'viewpoint'
 gem 'dotenv'
 
 
 group :development do
+  gem 'pry'
   gem "rake", "~> 10.0"
   gem "minitest", "~> 5.2"
   gem "rack-test", "~> 0.6"
--- booker.rb
+++ booker.rb
@@ -4,19 +4,59 @@ require 'dotenv'
 
 class Booker < Sinatra::Base
 
+  enable :sessions
   set :public_folder => "public", :static => true
 
-  use Rack::Auth::Basic, "Restricted Area" do |username, password|
-    endpoint = ENV.fetch('MS_EXCHANGE_SERVER_URL')
-    client   = Viewpoint::EWSClient.new(endpoint, username, password)
-    begin
-      !!client.folders
+  helpers do
+    def logged_in?
+      !!(session[:username] && session[:password])
+    end
+
+    def login(username, password)
+      session[:username] = username
+      session[:password] = password
+    end
+
+    def logout
+      session.delete(:username)
+      session.delete(:password)
+    end
+
+    def client(username, password)
+      endpoint = ENV.fetch('MS_EXCHANGE_SERVER_URL')
+      client   = Viewpoint::EWSClient.new(endpoint, username, password)
+    end
+
+    def authorized?(username, password)
+      !!client(username, password).folders
     rescue Viewpoint::EWS::Errors::UnauthorizedResponseError => e
       false
     end
   end
 
-  get "/" do
+  get '/' do
+    redirect to('/login') unless logged_in?
     erb :home
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    username = params[:username]
+    password = params[:password]
+    if authorized?(username, password)
+      login(username, password)
+      redirect to('/')
+    else
+      logout
+      redirect to('/login')
+    end
+  end
+
+  get '/logout' do
+    logout
+    erb :logout
   end
 end
